b8ac57b6089de8f7acdb3b4490a5b1d99dc0b0a6
Atila Boroš <atkerosbo@yahoo.com>
2025-01-09 11:08:34 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from sqlalchemy.orm import Session
